5a94210852d654b25fa99d796fd98ee15ce49e02
Aryan11-byte <aaryansureshnanda@gmail.com>
2026-03-29 16:22:44 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,14 +3,20 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebas
 import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 // 🔥 PASTE YOUR CONFIG HERE
+
 const firebaseConfig = {
-  apiKey: "YOUR_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "XXXX",
-  appId: "XXXX"
+  apiKey: "AIzaSyC0GJdupZV9b6N4bURmxu8erm5X8_7qlTw",
+  authDomain: "b-mart-c1785.firebaseapp.com",
+  projectId: "b-mart-c1785",
+  storageBucket: "b-mart-c1785.firebasestorage.app",
+  messagingSenderId: "201443603534",
+  appId: "1:201443603534:web:25f27ed4acc09506597f1f",
+  measurementId: "G-WKQ8FPB9ZV"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
